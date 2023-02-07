6e597e0db0f3c643664f5dd1e02065679cb48d39
rakify <irakibm@gmail.com>
2023-02-08 02:10:59 +0600

    finish touch

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,7 +105,7 @@ export default function Home() {
           style={{ backgroundImage: "url(/images/Left.png)", flex: 1 }}
         ></div>
 
-        <div style={{ flex: 20 }}>
+        <div style={{ flex: 20, paddingBottom: "50px" }}>
           <div
             className="bottomTitle bg-cover bg-center sm:w-[271px] md:w-[542px] sm:h-[56] md:h-[110px] mt-[107px] mb-[30px] text-2xl sm:text-3xl"
             style={{ backgroundImage: "url(/images/Brush.png)" }}
